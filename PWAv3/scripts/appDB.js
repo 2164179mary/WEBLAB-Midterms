926b8a01fba6de130e2ab616dbebddee3f5f330b
Kimberly Lopez-2161055 <2161055@slu.edu.ph>
2018-04-09 01:04:12 +0800

    PWA v3

---
+++ PWAv3/scripts/appDB.js
@@ -0,0 +1,50 @@
+var categories = [];
+var budget;
+var availability;
+var ambience = [];
+var modal = document.getElementById('filter-background');
+
+console.log(budgetQuery(budget));
+
+function getBudget(){
+    budget = document.getElementById("budget-value").value;
+    modal.style.display = "block";
+    console.log(budget);
+    return budget;
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function getCategory(id) {
+    var checkBox = document.getElementById(id);
+    if(checkBox.checked == true){
+        categories.push(id);
+        console.log(categories);
+    }if(checkBox.checked == false){
+        var index = categories.indexOf(id);
+        categories.splice(index, 1);
+        console.log(categories);
+    }
+}
+
+function getAmbience(id) {
+    var checkBox = document.getElementById(id);
+    if(checkBox.checked == true){
+        ambience.push(id);
+        console.log(ambience);
+    }if(checkBox.checked == false){
+        var index = ambience.indexOf(id);
+        ambience.splice(index, 1);
+        console.log(ambience);
+    }
+}
+
+function getAvailability(id) {
+    availability = document.getElementById(id).value;
+    console.log(availability);
+}
+
