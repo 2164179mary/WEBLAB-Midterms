00bf1ad14bf7591397e7ba408f745508886838de
Kimberly Lopez-2161055 <2161055@slu.edu.ph>
2018-04-10 02:40:55 +0800

    PWAv4

---
+++ PWAv4/New folder/scripts/sample.js
@@ -0,0 +1,12 @@
+document.getElementById("sample").innerHTML = menuList[1];
+/*var createMenu = function(){
+    var length = 10;
+    while(length != 0){
+        var para = document.createElement("p");
+        var node = document.createTextNode("random");
+        para.appendChild(node);
+        var element = document.getElementById("menuResult");
+        element.appendChild(para);
+        length--;
+    }
+}*/
